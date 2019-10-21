8523cc19e42b3b8a5a316594b85586558dadeec6
kanazawanao <naomiti3333@gmail.com>
2019-10-21 14:21:30 +0900

    ut impl

--- src/app/services/firestore/aria.service.spec.ts
+++ src/app/services/firestore/aria.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AriaService } from './aria.service';
+import { environment } from 'src/environments/environment';
 
 describe('AriaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFireAuthModule,
+      AngularFirestoreModule,
+    ]
+  }));
 
   it('should be created', () => {
     const service: AriaService = TestBed.get(AriaService);
--- src/app/services/firestore/auth.service.spec.ts
+++ src/app/services/firestore/auth.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFireAuthModule,
+      AngularFirestoreModule,
+    ]
+  }));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
